c88731b983d5752dcd6a63f291f1a1c490e91390
Liubov Pashkova <liupa@dtu.dk>
2024-04-09 14:09:00 +0000

    PANKB-148 Added a model for organisms

--- organisms/models.py
+++ organisms/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+# Model representing info on the Organisms page: ----
+class Organisms(models.Model):
+   _id = models.CharField(max_length=24, primary_key=True)
+   family = models.CharField(max_length=30)
+   species = models.CharField(max_length=40)
+   openness = models.CharField(max_length=6)   # ('Open' or 'Closed'), used CharField here instead of boolean to facilitate the parsing
+   gene_class_distribution = models.CharField(max_length=20)
+   genomes_num = models.IntegerField()
+   pangenome_analyses = models.CharField(max_length=40)
+
+   class Meta:
+       managed = True  # tells Django not to manage the table’s creation, modification, and deletion
+       db_table = 'organisms'
