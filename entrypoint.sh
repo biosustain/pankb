15ed67bccb3f47e4e0426d42bc269c3d6ad2e18d
Liubov Pashkova <liupa@dtu.dk>
2024-05-22 17:30:40 +0000

    Increased nginx and gunicorn timeouts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
+gunicorn django_project.wsgi:application --bind 0.0.0.0:8000 --timeout 1800
