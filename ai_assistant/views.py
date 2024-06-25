39b64aa8e7139752516ac035cad7c300035f9796
Liubov Pashkova <liupa@dtu.dk>
2024-06-25 18:23:36 +0000

    Added URL of the AI Assistant App as an env file parameter

--- ai_assistant/views.py
+++ ai_assistant/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+import os
 
 def ai_assistant(request):
   template = loader.get_template('ai_assistant/ai_assistant.html')
-  return HttpResponse(template.render())
+  # The url of the separately deployed AI Assistant web application is included into the render context
+  # as a env variable: ----
+  return HttpResponse(template.render(context={'AI_ASSISTANT_APP_URL': os.environ.get('AI_ASSISTANT_APP_URL')}))
