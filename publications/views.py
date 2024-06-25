7cc848dea5e393b806cc881f4620b797eb324582
Liubov Pashkova <liupa@dtu.dk>
2024-06-25 23:36:35 +0000

    Changed the publications in accordance with the new list used by the GenAI Bot

--- publications/views.py
+++ publications/views.py
@@ -8,9 +8,13 @@ import pandas as pd
 # Template renderer for the Publications page
 def publications(request):
   template = loader.get_template('publications/Publications.html')
-  url = 'https://pankb.blob.core.windows.net/data/PanKB/web_data/publications.csv'    # the url of the respective json file stored on the Microsoft Azure Blob Storage
+  # The script producing the csv file with the list of pulications
+  # is contained under /misc subfolder
+  # and may not be included under the version control
+  # contact liupa@dtu.dk for the questions.
+  url = 'https://pankb.blob.core.windows.net/data/PanKB/web_data/publications_v2_short.csv'
   r = requests.get(url)
-  dataset_df = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+  dataset_df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), sep='\t')   # better never use comma as a separator here (as words in titles and authors' names can be separated by commas)
   dataset_dict = dataset_df.to_dict(orient='records')
   # Compose a context for the template rendering
   context = {
