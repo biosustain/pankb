9d5b32af57aee0a753ace4dc0bf2119be3e868bd
Pascal A. Pieters <paspie@biosustain.dtu.dk>
2024-10-10 06:48:21 +0000

    Removed all iframes from homepage, all plots are now in the same page. Homepage plot data is retreived from the latest entry in the pankb_stats database. Upped django version to 4.1 (we should go even higher, but djongo blocks this).

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Model representing info on the Organisms page: ----
+class Statistics(models.Model):
+   _id = models.CharField(max_length=24, primary_key=True)
+   date = models.DateTimeField()
+   pankb_dimensions = models.JSONField()
+   species_genome_gene = models.JSONField()
+   organism_genome_count = models.JSONField()
+   organism_gene_count = models.JSONField()
+   treemap = models.JSONField()
+
+   class Meta:
+       managed = True  # tells Django not to manage the table’s creation, modification, and deletion
+       db_table = 'pankb_stats'
--- home/views.py
+++ home/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 from django.template import loader
+from .models import Statistics
+import json
 
 def home(request):
   template = loader.get_template('home/index.html')
-  return HttpResponse(template.render())
 
+  stats = Statistics.objects.latest("date")
 
+  # Compose the render context: ----
+  context = {
+    'dimension_dataset': json.dumps(stats.pankb_dimensions),
+    'organism_genome_dataset': json.dumps(stats.organism_genome_count),
+    'organism_gene_dataset': json.dumps(stats.organism_gene_count),
+    'genome_gene_dataset': json.dumps(stats.species_genome_gene),
+    'treemap_dataset': json.dumps(stats.treemap)
+  }
 
+  return HttpResponse(template.render(context, request))
+  
