bd5ed8da473462418b0fa4efcce2ac2928bc7356
Pascal A. Pieters <paspie@biosustain.dtu.dk>
2024-12-03 15:46:32 +0000

    New publications page

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Publication(models.Model):
+    _id = models.CharField(max_length=24, primary_key=True)
+    source = models.CharField(max_length=100)
+    title = models.CharField(max_length=400)
+
+    class Meta:
+        managed = False  # tells Django to manage the table’s creation, modification, and deletion
+        db_table = 'pankb_publications'
--- publications/views.py
+++ publications/views.py
@@ -3,22 +3,18 @@ from django.http import HttpResponse
 from django.template import loader
 from django.conf import settings
 import json, requests, io
-import pandas as pd
+from .models import Publication
 
 
 # Template renderer for the Publications page
 def publications(request):
   template = loader.get_template('publications/publications.html')
-  # The script producing the csv file with the list of pulications
-  # is contained under /misc subfolder
-  # and may not be included under the version control
-  # contact liupa@dtu.dk for the questions.
-  url = settings.AZURE_WEB_DATA_URL + 'publications_v2_short.csv'
-  r = requests.get(url)
-  dataset_df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), sep='\t')   # better never use comma as a separator here (as words in titles and authors' names can be separated by commas)
-  dataset_dict = dataset_df.to_dict(orient='records')
+
+  publications = Publication.objects.filter().values("title", "source")
+  publications = list(publications)
+  
   # Compose a context for the template rendering
   context = {
-    'dataset': json.dumps(dataset_dict)
+    'dataset': json.dumps(publications)
   }
   return HttpResponse(template.render(context, request))
