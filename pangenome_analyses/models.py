5f822f941243c92f1941ee889b9bbfe169c1849b
Dharam Kapila <dharamkapila@gmail.com>
2025-08-08 13:40:13 +0200

    Add db interop apis (#17)
    * Add db interop apis
    
    * Remove the model file from db interop
    
    ---------
    
    Co-authored-by: Pascal <paspie@biosustain.dtu.dk>

--- pangenome_analyses/models.py
+++ pangenome_analyses/models.py
@@ -4,6 +4,16 @@ from common import database
 class GeneAnnotations:
     objects = database.MongoDBObjects('pankb_gene_annotations')
     
-    class NotFound(Exception):
-        pass
-    
+    def get_gene_analysis_pairs(genes):
+        """
+        Return all distinct (gene, pangenome_analysis) tuples
+        for the provided list of gene names.
+        """
+        cursor = GeneAnnotations.objects.find(
+            {"gene": {"$in": genes}},
+            projection=["gene", "pangenome_analysis"]
+        )
+        pairs = {(doc["gene"], doc["pangenome_analysis"]) for doc in cursor}
+        if not pairs:
+            raise GeneAnnotations.NotFound("No matching gene annotations found")
+        return list(pairs)
