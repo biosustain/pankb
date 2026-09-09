75213ed25f2f3a721bc381c4faa3e889f9c9c2da
SHINTEKI <wuxindi1127@gmail.com>
2026-09-09 09:09:47 +0000

    add version number to Swagger UI's URL

--- django_project/urls.py
+++ django_project/urls.py
@@ -194,6 +194,6 @@ urlpatterns = [
     path("interop-query/query-by-pair", interop_views.query_by_pair, name="query_by_pair"),
     
     path('api/v1/schema/', SpectacularAPIView.as_view(api_version='v1'), name='schema-v1'),
-    path('api/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema-v1'), name='swagger-ui'),
+    path('api/v1/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema-v1'), name='swagger-ui'),
     # path('admin/', admin.site.urls)   # make the amdin panel inaccessible via its utl (the admin admin is preserved for the potential future needs)
 ]
